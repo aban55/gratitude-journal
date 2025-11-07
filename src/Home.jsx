a1d4cfd557288adc2c454324932f76b416bd6ea1
aban55 <abgenuse@gmail.com>
2025-11-07 13:08:35 +0530

    migrate to Google Identity Services (GIS)

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+// src/Home.jsx
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>🌿 Welcome to Gratitude Journal</h1>
+      <p>
+        This is your main journal screen. Use the navigation to explore or
+        start writing.
+      </p>
+    </div>
+  );
+}
